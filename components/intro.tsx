aaafa49b2e8626763fc9a2a5cc2fd4f6aa742af5
TrongNhan30520 <trongnhan30520@gmail.com>
2023-09-29 23:51:07 +0700

    update

--- components/intro.tsx
+++ components/intro.tsx
@@ -9,7 +9,7 @@ import { HiDownload } from "react-icons/hi";
 import { FaGithubSquare } from "react-icons/fa";
 import { useSectionInView } from "@/lib/hooks";
 import { useActiveSectionContext } from "@/context/active-section-context";
-import MyPic from '@/public/images/myPic.png'
+import MyPic from '@/public/images/mypic.png'
 
 export default function Intro() {
   const { ref } = useSectionInView("Home", 0.5);
